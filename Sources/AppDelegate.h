fe52423e077b690c1b2fa226c7050ae4a8c4cf74
Amr Aboelela <amraboelela@gmail.com>
2016-08-17 17:42:47 -0700

    Added lots of files

---
+++ Sources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectiveCLevelDBTest
+//
+//  Created by Amr Aboelela on 8/17/16.
+//  Copyright © 2016 Amr Aboelela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sources/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  ObjectiveCLevelDBTest
+//
+//  Created by Amr Aboelela on 8/17/16.
+//  Copyright © 2016 Amr Aboelela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController {
+    //LevelDB *db;
+}
+
+@end
+
